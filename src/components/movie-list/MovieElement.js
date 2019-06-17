014d4b8185b07d6c009c33dac92ff8b6b69664b5
fabiengoyhex <fabien.goyhex@gmail.com>
2019-06-17 15:03:17 +0200

    added card on movieElement

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 
 export default class MovieElement extends Component {
 	render() {
-		return <div>movie Elements</div>;
+		return (
+			<div className='w-25 p-2'>
+				<div className='card'>
+					<img src='' className=' card-img-top' />
+					<div className=' card-body'>
+						<h5 className='card-title'>t</h5>
+						<p className='card-text'>par</p>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
+import MovieElement from './MovieElement';
 
 export default class MovieList extends Component {
 	render() {
 		return (
 			<div className='w-75 d-flex flex-row flex-wrap align-content-start'>
-				<div className='w-25 border p-2'>
-					<div className=' border'> film</div>
-				</div>
-				<div className='w-25 border p-2'>
-					<div className=' border'> film</div>
-				</div>
-				<div className='w-25 border p-2'>
-					<div className=' border'> film</div>
-				</div>
-				<div className='w-25 border p-2'>
-					<div className=' border'> film</div>
-				</div>
-				<div className='w-25 border p-2'>
-					<div className=' border'> film</div>
-				</div>
-				<div className='w-25 border p-2'>
-					<div className=' border'> film</div>
-				</div>
+				<MovieElement />
+				<MovieElement />
+				<MovieElement />
+				<MovieElement />
+				<MovieElement />
+				<MovieElement />
 			</div>
 		);
 	}
